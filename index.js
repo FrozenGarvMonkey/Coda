ed8a8b3675021e6a0c7e7d0119dfc5d19143f6f7
Garv Nair <32934519+garvnair@users.noreply.github.com>
2021-09-16 03:17:51 +0800

    Just a lil bit of modifications and hidden token

--- index.js
+++ index.js
@@ -9,53 +9,58 @@ const ffmpeg = require("ffmpeg-static");
 const voice = require("@discordjs/voice");
 const DisTube = require("distube").default;
 const https = require('https-proxy-agent');
+
 const client = new Discord.Client({
-    //fetchAllMembers: false,
-    //restTimeOffset: 0,
-    //restWsBridgetimeout: 100,
-    shards: "auto",
-    //shardCount: 5,
-    allowedMentions: {
-      parse: [ ],
-      repliedUser: false,
+  //fetchAllMembers: false,
+  //restTimeOffset: 0,
+  //restWsBridgetimeout: 100,
+  shards: "auto",
+  //shardCount: 5,
+  allowedMentions: {
+    parse: [],
+    repliedUser: false,
+  },
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+  intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    //Discord.Intents.FLAGS.GUILD_MEMBERS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+    Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+    //Discord.Intents.FLAGS.GUILD_BANS,
+    //Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    //Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+    //Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+    //Discord.Intents.FLAGS.GUILD_INVITES,
+    //Discord.Intents.FLAGS.GUILD_PRESENCES,
+    //Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    //Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    //Discord.Intents.FLAGS.DIRECT_MESSAGES,
+    //Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    //Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
+  ],
+  presence: {
+    activity: {
+      name: `+help | musicium.eu`,
+      type: "PLAYING",
     },
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-    intents: [ 
-        Discord.Intents.FLAGS.GUILDS,
-        //Discord.Intents.FLAGS.GUILD_MEMBERS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES,
-        Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-        //Discord.Intents.FLAGS.GUILD_BANS,
-        //Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-        //Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
-        //Discord.Intents.FLAGS.GUILD_WEBHOOKS,
-        //Discord.Intents.FLAGS.GUILD_INVITES,
-        //Discord.Intents.FLAGS.GUILD_PRESENCES,
-        //Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        //Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
-        //Discord.Intents.FLAGS.DIRECT_MESSAGES,
-        //Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-        //Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
-    ],
-    presence: {
-      activity: {
-        name: `+help | musicium.eu`, 
-        type: "PLAYING", 
-      },
-      status: "online"
-    }
+    status: "online"
+  }
 });
 //BOT CODED BY: Tomato#6966
 //DO NOT SHARE WITHOUT CREDITS!
 //const proxy = 'http://123.123.123.123:8080';
 //const agent = https(proxy);
-const { SpotifyPlugin } = require("@distube/spotify");
-const { SoundCloudPlugin } = require("@distube/soundcloud");
+const {
+  SpotifyPlugin
+} = require("@distube/spotify");
+const {
+  SoundCloudPlugin
+} = require("@distube/soundcloud");
 let spotifyoptions = {
   parallel: true,
   emitEventsAfterFetching: true,
 }
-if(config.spotify_api.enabled){
+if (config.spotify_api.enabled) {
   spotifyoptions.api = {
     clientId: config.spotify_api.clientId,
     clientSecret: config.spotify_api.clientSecret,
@@ -70,7 +75,7 @@ client.distube = new DisTube(client, {
   emitAddSongWhenCreatingQueue: false,
   //emitAddListWhenCreatingQueue: false,
   searchSongs: 0,
-  youtubeCookie: config.youtubeCookie,     //Comment this line if you dont want to use a youtube Cookie 
+  youtubeCookie: config.youtubeCookie, //Comment this line if you dont want to use a youtube Cookie 
   nsfw: true, //Set it to false if u want to disable nsfw songs
   emptyCooldown: 25,
   ytdlOptions: {
@@ -99,18 +104,25 @@ client.aliases = new Discord.Collection();
 client.categories = require("fs").readdirSync(`./commands`);
 client.allEmojis = require("./botconfig/emojis.json");
 
-client.setMaxListeners(100); require('events').defaultMaxListeners = 100;
+client.setMaxListeners(100);
+require('events').defaultMaxListeners = 100;
 
-client.settings = new Enmap({ name: "settings",dataDir: "./databases/settings"});
-client.infos = new Enmap({ name: "infos", dataDir: "./databases/infos"});
+client.settings = new Enmap({
+  name: "settings",
+  dataDir: "./databases/settings"
+});
+client.infos = new Enmap({
+  name: "infos",
+  dataDir: "./databases/infos"
+});
 
 
 //Require the Handlers                  Add the antiCrash file too, if its enabled
 ["events", "commands", "slashCommands", settings.antiCrash ? "antiCrash" : null, "distubeEvent"]
-    .filter(Boolean)
-    .forEach(h => {
-        require(`./handlers/${h}`)(client);
-    })
+.filter(Boolean)
+  .forEach(h => {
+    require(`./handlers/${h}`)(client);
+  })
 //Start the Bot
 client.login(config.token)
 
